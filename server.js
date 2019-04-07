c005c1bbc7fc7296427d63e3bd17c77e528cdc9c
Ryan <supermarioryan@gmail.com>
2019-04-07 18:47:50 +0100

    Enable compression

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const temp = require('temp').track()
 const tempFolder = temp.mkdirSync()
 const { createServer } = require('http')
-
+const compression = require('compression')
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
@@ -11,6 +11,7 @@ const handle = nextApp.getRequestHandler()
 
 nextApp.prepare().then(() => {
   const app = express()
+  app.use(compression())
   const server = createServer(app)
 
   app.use('/api', require('./api/router')(tempFolder))
